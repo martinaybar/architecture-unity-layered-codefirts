64362b69a8136dcba86988cbd0cbff92f23ebda8
martinaybar <martinaybar@gmail.com>
2016-04-12 10:17:44 -0300

    Add project files.

---
+++ architecture-unity-layered-codefirts/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace architecture_unity_layered_codefirts
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ architecture-unity-layered-codefirts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="architecture_unity_layered_codefirts.MvcApplication" Language="C#" %>
