bb0a408a7588a9de225a8bc10b65e605f7767c2c
martinaybar <martinaybar@gmail.com>
2016-04-15 18:04:06 -0300

    automapper

---
+++ Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Entities;
+using Web.Models;
+
+
+namespace Web.App_Start
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+        }
+    }
+}
